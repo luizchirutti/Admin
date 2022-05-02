eb9664e4c8a3f0fea99ce78b2e9b6af799242e7b
Luiz Chirutti <luizchirutti@gmail.com>
2022-05-02 16:03:41 -0300

    add adminBro

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const bodyParser = require('body-parser')
+const AdminBro = require('admin-bro')
+const AdminBroExpressjs = require('admin-bro-expressjs')
+​
+// We have to tell AdminBro that we will manage mongoose resources with it
+AdminBro.registerAdapter(require('admin-bro-mongoose'))
 ​
 // express server definition
 const app = express()
@@ -28,7 +33,7 @@ const Article = mongoose.model('Article', artcileSchema);
 // Routes definitions
 app.get('/', (req, res) => res.send('Hello World!'))
 ​
-// Route which returns users
+// Route which returns last 100 users from the database
 app.get('/users', async (req, res) => {
  const users = await User.find({}).limit(10)
  res.send(users)
@@ -45,6 +50,16 @@ app.get('/articles', async (req, res) => {
  const articles = await Article.find({}).limit(10)
  res.send(articles)
 })
+​
+// Pass all configuration settings to AdminBro
+const adminBro = new AdminBro({
+ resources: [User, Article],
+ rootPath: '/admin',
+})
+​
+// Build and use a router which will handle all AdminBro routes
+const router = AdminBroExpressjs.buildRouter(adminBro)
+app.use(adminBro.options.rootPath, router)
 ​
 // Running the server
 const run = async () => {
