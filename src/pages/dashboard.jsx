793c11e277ce5d4a7a69084c84fe1317ac18ca93
Luiz Chirutti <luizchirutti@gmail.com>
2022-05-02 17:23:50 -0300

    add dashboard

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { WrapperBox } from 'admin-bro/components'
+​
+const Dashboard = (props) => {
+ return (
+   <WrapperBox>
+     <h1>Meu Dashboard Customizado</h1>
+   </WrapperBox>
+)
+}
+​
+export default Dashboard;
+
